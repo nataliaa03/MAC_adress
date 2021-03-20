f12dd5132a50ae3264681dbf3b2bb36c5e9432df
natalka <natalkalopata@gmail.com>
2021-03-21 00:17:19 +0100

    first comit

---
+++ MACadress.py
@@ -0,0 +1,24 @@
+import requests
+import sys
+import os
+import json
+
+API_KEY = os.environ.get('MAC_ADRESS_API_KEY')
+OUTPUT = 'json' #possible other formats: xml, csv, vendor (more info: https://macaddress.io/api/documentation/making-requests)
+
+def get_data():
+    headers = {'Content-Type': 'application/json'}
+    mac_address = str(sys.argv[1]) #user's argument in cmd
+    url = r'https://api.macaddress.io/v1?apiKey={}&output={}&search={}'.format(API_KEY, OUTPUT, mac_address)
+    print("URL: ", url)
+    response = requests.get(url=url, headers=headers)
+    json_data = response.json() 
+    company_name = json_data["vendorDetails"]["companyName"]
+    if company_name == '':
+        print("No company name assigned to this MAC adress.") #taking the value of the key: company name
+    else:
+        print("COMPANY NAME: ", company_name)
+
+
+if __name__ == "__main__":
+    get_data()
